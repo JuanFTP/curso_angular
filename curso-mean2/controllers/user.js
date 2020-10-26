31d965a3f55498864872179ec75f0c27d6bab7af
JuanFTP <juanftp10@gmail.com>
2020-10-26 16:21:34 -0600

    Lección 21 | Middleware de autenticación (JWT)

--- curso-mean2/controllers/user.js
+++ curso-mean2/controllers/user.js
@@ -1,12 +1,11 @@
 'use-strict'
 
 var bcrypt = require('bcrypt-nodejs');
-var User = require('../models/user')
+var User = require('../models/user');
+var jwt = require('../services/jwt');
 
 function pruebas(req, res){
-	res.status(200).send({
-		mensaje: "Probando una acción del controlador del Usuario del API REST con Node y MongoDB"
-	});
+	res.status(200).send({message: "Probando una acción del controlador del Usuario del API REST con Node y MongoDB"});
 }
 
 function saveUser(req, res) {
@@ -44,11 +43,44 @@ function saveUser(req, res) {
 			}
 		});
 	} else {
-		res.status(200).send({message: 'Introduce la contraseña'});
+		res.status(200).send({message: 'Introduce una contraseña'});
 	}
 };
 
+function loginUser(req, res) {
+	var params = req.body;
+
+	var email = params.email;
+	var password = params.password;
+
+	User.findOne({email: email.toLowerCase()}, (err, user) => {
+		if(err) {
+			res.status(500).send({message:'Error en la patición'});
+		} else {
+			if(!user) {
+				res.status(404).send({message:'El usuario no existe'});
+			} else {
+				// Comprobar la contraseña
+				bcrypt.compare(password, user.password, function(err, check) {
+					if(check) {
+						// Devolver los datos del usuario logueado
+						if(params.gethash) {
+							// Devolver un token de JWT para autenticar al usuario
+							res.status(200).send({token: jwt.createToken(user)});
+						} else {
+							res.status(200).send({message: 'getHash false', user});
+						}
+					} else {
+						res.status(404).send({message:'El usuario no ha podido loguearse'});
+					}
+				});
+			}
+		}
+	});
+}
+
 module.exports = {
 	pruebas,
-	saveUser
+	saveUser,
+	loginUser
 };
