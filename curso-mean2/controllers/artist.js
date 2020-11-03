fb66990047def551407d4ffe5c34aad868a86b74
JuanFTP <juanftp10@gmail.com>
2020-11-03 14:30:37 -0600

    Lección 26 | Crear un artista

--- curso-mean2/controllers/artist.js
+++ curso-mean2/controllers/artist.js
@@ -11,6 +11,28 @@ function getArtist(req, res) {
 	res.status(200).send({message: 'Método getArtist del controlador artist.js'});	
 }
 
+function saveArtist(req, res) {
+	var artist = new Artist();
+
+	var params  =req.body;
+	artist.name = params.name;
+	artist.description = params.description;
+	artist.image = 'null';
+
+	artist.save((err, artistStore) => {
+		if(err) {
+			res.status(500).send({message: 'Error al guardar el mensaje'});
+		} else {
+			if(!artistStore) {
+				res.status(404).send({message: 'El artista no ha sido guardado'});
+			} else {
+				res.status(200).send({message: 'Artista guardado exitosamente', artist: artistStore});
+			}
+		}
+	});
+}
+
 module.exports = {
-	getArtist
+	getArtist,
+	saveArtist
 };
