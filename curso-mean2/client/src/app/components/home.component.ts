47a0464971f5c240c6c4fed7a67b3c92b53cdee3
JuanFTP <juanftp10@gmail.com>
2020-12-23 16:07:20 -0600

    JFTP | Ajuste del player para dimensiones más pequeñas e inicialización de integración para página home

--- curso-mean2/client/src/app/components/home.component.ts
+++ curso-mean2/client/src/app/components/home.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Artist } from '../models/artist';
+import { Album } from '../models/album';
+import { Song } from '../models/song';
+import { ArtistService } from '../services/artist.service';
+import { AlbumService } from '../services/album.service';
+import { SongService } from '../services/song.service';
 
 @Component({
 	selector: 'home',
-	templateUrl: './../views/home.html'
+	templateUrl: './../views/home.html',
+	providers: [
+		ArtistService,
+		AlbumService,
+		SongService
+	]
 })
 
 export class HomeComponent implements OnInit {
 	public title: string;
+	public artists: Artist[];
+	public albums: Album[];
+	public songs: Song[];
 
 	ngOnInit() {
 		console.log("home.component cargado...");
@@ -18,7 +32,5 @@ export class HomeComponent implements OnInit {
 		private _router: Router
 	) {
 		this.title  = "Inicio";
-
-		// Validar sesión, si no se ha loggueado regresarlo al login
 	}
 }
