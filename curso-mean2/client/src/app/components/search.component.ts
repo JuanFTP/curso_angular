d426a8550f38932fcf2bf789b06d4ae6e0a0b504
JuanFTP <juanftp10@gmail.com>
2020-12-23 13:03:58 -0600

    JFTP | Se añade componente se búsqueda

--- curso-mean2/client/src/app/components/search.component.ts
+++ curso-mean2/client/src/app/components/search.component.ts
@@ -8,13 +8,21 @@ import { GLOBAL } from '../services/global';
 })
 
 export class SearchComponent implements OnInit {
+	public title: string;
 	public url: string;
+	public search: string;
 
 	public constructor() {
 		this.url = GLOBAL.url;
+		this.title = "Buscar";
+		this.search = "";
 	}
 
 	public ngOnInit() {
 		console.log("search.component cargado...");
+	}
+
+	public onSearch() {
+		console.log(this.search);
 	}
 }
