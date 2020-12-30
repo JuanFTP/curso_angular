ad993aacac956a86328b41dd4ebaa89eb6567dd6
JuanFTP <juanftp10@gmail.com>
2020-12-29 19:24:30 -0600

    JFTP | Player integrado y carga de música completada, por corregir

--- curso-mean2/client/src/app/components/player.component.ts
+++ curso-mean2/client/src/app/components/player.component.ts
@@ -12,9 +12,13 @@ import { PlayerService } from '../services/player.service';
 export class PlayerComponent implements OnInit {
 	public url: string;
 	public song: Song;
+	public playerSongSource: any;
+	public playerSongControls: any;
 	public playerSongImage: any;
 	public playerSongTitle: any;
 	public playerSongArtist: any;
+	public playerSongFile: any;
+	public playerVolume: number;
 
 	public message: string;
 
@@ -27,6 +31,9 @@ export class PlayerComponent implements OnInit {
 		this.playerSongImage = "5HifqDvtIu6Z6GXmzoMxq-X0.jpg";
 		this.playerSongTitle = "Ninguna canción seleccionada";
 		this.playerSongArtist = "Sin artísta";
+		this.playerSongSource = null;
+		this.playerSongControls = null;
+		this.playerVolume = 0.6;
 	}
 
 	public ngOnInit() {
@@ -46,14 +53,37 @@ export class PlayerComponent implements OnInit {
 			this.playerSongImage = this.song.album.image;
 			this.playerSongTitle = this.song.name;
 			this.playerSongArtist = this.song.album.artist.name;
+			this.playerSongFile = this.song.file;
+			this.playerSongControls = document.getElementById("player-controls");
+			this.playerSongSource = document.getElementById("player-source");
 		}
 	}
 
+	public setSource() {
+		this.playerSongSource.setAttribute("src", this.playerSongFile);
+	}
+
 	public playSong() {
-		console.log("playSong");
+		this.playerSongControls.play();
 	}
 
 	public pauseSong() {
-		console.log("pauseSong");
+		this.playerSongControls.pause();
+	}
+
+	public volumeUp() {
+		if(this.playerVolume < 1) {
+			this.playerVolume += 0.1;
+			this.playerSongControls.volume = this.playerVolume;
+			console.log(this.playerVolume);			
+		}
+	}
+
+	public volumeDown() {
+		if(this.playerVolume > 0.1) {
+			this.playerVolume -= 0.1;
+			this.playerSongControls.volume = this.playerVolume;
+			console.log(this.playerVolume);
+		}	
 	}
 }
